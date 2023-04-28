7db0ac73e0e1c1a7f1fa2a3a2f9982e9986d5c81
Efeoseaje <efeoseaje@yahoo.com>
2023-04-28 19:42:57 +0100

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -18,17 +18,14 @@ void bubble_sort(int *array, size_t size)
 	for (i = 0; i < size - 1; i++)
 	{
 		flag = 0;
+		for (j = 0; j < size - 1 - i; j++)
 		{
-			for (j = 0; j < size - 1 - i; j++)
+			if (array[j] > array[j + 1])
 			{
-				if (array[j] > array[j + 1])
-				{
-					temp = array[j];
-					array[j] = array[j + 1];
-					array[j + 1] = temp;
-					print_array(array, size);
-					flag = 1;
-				}
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 		if (flag == 0)
